864ecada74a326c0f9febdad2aabf5daadbe0dad
Niraj Gns <nirajgns@gmail.com>
2025-03-20 19:47:07 +0545

    created schemas required for authjs and implemented user seed

--- db/seed.ts
+++ db/seed.ts
@@ -2,8 +2,10 @@ import { prisma } from "./prisma";
 import sampleData from "./sample-data";
 
 async function main() {
-  await prisma.product.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.user.createMany({ data: sampleData.users });
 
+  await prisma.product.deleteMany();
   await prisma.product.createMany({ data: sampleData.products });
 
   console.log("Data seeded successfully");
